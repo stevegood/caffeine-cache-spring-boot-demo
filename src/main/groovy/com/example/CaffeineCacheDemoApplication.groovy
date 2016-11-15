59bbb10732047eee25b9bf461b4a73df9d4ee1b3
Steve Good <steve.good@am.sony.com>
2016-11-15 14:27:47 -0800

    Initial commit

---
+++ src/main/groovy/com/example/CaffeineCacheDemoApplication.groovy
@@ -0,0 +1,31 @@
+package com.example
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.cache.annotation.Cacheable
+import org.springframework.cache.annotation.EnableCaching
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.RequestMapping
+
+@SpringBootApplication
+@Controller
+@EnableCaching
+class CaffeineCacheDemoApplication {
+
+	@Autowired
+	TimeService timeService
+
+	static void main(String[] args) {
+		SpringApplication.run CaffeineCacheDemoApplication, args
+	}
+
+	@RequestMapping
+	def index(Model model) {
+		Map attrs = [start: timeService.timeNow, cached: timeService.cachedTime]
+		model.addAllAttributes attrs
+		'index'
+	}
+
+}
---
+++ src/main/groovy/com/example/TimeService.groovy
@@ -0,0 +1,22 @@
+package com.example
+
+import org.springframework.cache.annotation.Cacheable
+import org.springframework.stereotype.Service
+
+/**
+ * Created by sgood on 11/15/16.
+ */
+@Service
+class TimeService {
+
+    long getTimeNow() {
+        new Date().time
+    }
+
+    @Cacheable('timeCached')
+    long getCachedTime() {
+        timeNow
+    }
+
+
+}
